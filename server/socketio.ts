dd747890c6b069af302a219514f6d59d159a445a
Sara Cohen <sc0583218777@n-b-solutions.com>
2024-09-15 15:26:12 +0300

    split app.ts

---
+++ server/socketio.ts
@@ -0,0 +1,48 @@
+import { IncomingMessage, Server as HttpServer, ServerResponse } from "http";
+import { Server as SocketioServer } from "socket.io";
+
+import { CronJob } from "cron";
+
+export const initSocketio = (
+  server: HttpServer<typeof IncomingMessage, typeof ServerResponse>
+) => {
+  const io = new SocketioServer(server, {
+    cors: {
+      origin: process.env.CLIENT_URL,
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A user connected " + socket.id);
+
+    // socket.on("changeRoomStatus", async ({ nameRoom, newStatus }) => {
+    //   console.log(" socket.on");
+    //   try {
+    //     const updatedStatuses1 =
+    //       await ScheduleController.setRoomSteadyFlagAndStatus(
+    //         nameRoom,
+    //         newStatus
+    //       );
+    //     io.emit("roomStatusUpdated", updatedStatuses1);
+    //   } catch (error) {
+    //     console.error("Error handling changeRoomStatus event:", error);
+    //   }
+    // });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+    });
+  });
+
+  //   const job = new CronJob("* * * * *", async () => {
+  //     console.log("Running cron job to update rooms...");
+  //     const updatedStatuses = await ScheduleController.updateRoomStatuses();
+
+  //     await ScheduleController.logBeforeShkiah();
+  //     io.emit("roomStatusUpdated", updatedStatuses);
+  //   });
+
+  //   job.start();
+  return io;
+};
