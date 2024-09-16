b4efefc5247cb39962e15abeb3a76cf5bf18fb41
Sara Cohen <sc0583218777@n-b-solutions.com>
2024-09-16 13:27:24 +0300

    fix split app

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { createServer } from "http";
 
+import connectDB from "./DB/mongoConnect";
+import { initSocketio } from "./socketio";
 import { router } from "./router";
 
 const app = express();
+
+//connect to DB
+connectDB();
+//create server
+const server = createServer(app);
+//connect to socket io
+initSocketio(server);
 
 // set security HTTP headers
 app.use(helmet());
@@ -15,5 +25,9 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
 app.use("/", router);
+
+server.listen(4000, () => {
+    console.log("Server is running on port 4000");
+  });
 
 export const viteNodeApp = app;
--- server/socketio.ts
+++ server/socketio.ts
@@ -3,6 +3,8 @@ import { Server as SocketioServer } from "socket.io";
 
 import { CronJob } from "cron";
 
+import ScheduleController from'./controller/scheduleController'
+
 export const initSocketio = (
   server: HttpServer<typeof IncomingMessage, typeof ServerResponse>
 ) => {
@@ -16,33 +18,33 @@ export const initSocketio = (
   io.on("connection", (socket) => {
     console.log("A user connected " + socket.id);
 
-    // socket.on("changeRoomStatus", async ({ nameRoom, newStatus }) => {
-    //   console.log(" socket.on");
-    //   try {
-    //     const updatedStatuses1 =
-    //       await ScheduleController.setRoomSteadyFlagAndStatus(
-    //         nameRoom,
-    //         newStatus
-    //       );
-    //     io.emit("roomStatusUpdated", updatedStatuses1);
-    //   } catch (error) {
-    //     console.error("Error handling changeRoomStatus event:", error);
-    //   }
-    // });
+    socket.on("changeRoomStatus", async ({ nameRoom, newStatus }) => {
+      console.log(" socket.on");
+      try {
+        const updatedStatuses1 =
+          await ScheduleController.setRoomSteadyFlagAndStatus(
+            nameRoom,
+            newStatus
+          );
+        io.emit("roomStatusUpdated", updatedStatuses1);
+      } catch (error) {
+        console.error("Error handling changeRoomStatus event:", error);
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log("User disconnected");
     });
   });
 
-  //   const job = new CronJob("* * * * *", async () => {
-  //     console.log("Running cron job to update rooms...");
-  //     const updatedStatuses = await ScheduleController.updateRoomStatuses();
+    const job = new CronJob("* * * * *", async () => {
+      console.log("Running cron job to update rooms...");
+      const updatedStatuses = await ScheduleController.updateRoomStatuses();
 
-  //     await ScheduleController.logBeforeShkiah();
-  //     io.emit("roomStatusUpdated", updatedStatuses);
-  //   });
+      await ScheduleController.logBeforeShkiah();
+      io.emit("roomStatusUpdated", updatedStatuses);
+    });
 
-  //   job.start();
+    job.start();
   return io;
 };
