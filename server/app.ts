82178d4f6e9e8ffd25254f679a2112a72bc05af4
Sara Cohen <sc0583218777@n-b-solutions.com>
2024-09-17 08:46:12 +0300

    fix app.ts

--- server/app.ts
+++ server/app.ts
@@ -18,7 +18,9 @@ initSocketio(server);
 
 // set security HTTP headers
 app.use(helmet());
+// parse json request body
 app.use(express.json({ limit: "50mb" }));
+// parse urlencoded request body
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cors());
 
