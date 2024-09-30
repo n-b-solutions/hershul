e05759c152ef40f2a03b6ecea7f90164cdd435c9
Noam Ben Shabat <nbs0556771785@n-b-solutions.com>
2024-09-30 11:37:13 +0300

    fix server hmr (vite) and mongo double connection

--- server/app.ts
+++ server/app.ts
@@ -1,33 +1,36 @@
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import { createServer } from "http";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { createServer } from 'http';
 
-import connectDB from "./DB/mongoConnect";
-import { initSocketio } from "./socketio";
-import { router } from "./router";
+import connectDB from './DB/mongoConnect';
+import { initSocketio } from './socketio';
+import { router } from './router';
 
 const app = express();
+const server = createServer(app);
 
 //connect to DB
-connectDB();
-//create server
-const server = createServer(app);
+connectDB().catch((err) => console.log(err));
+
 //connect to socket io
 initSocketio();
 
 // set security HTTP headers
 app.use(helmet());
 // parse json request body
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }));
 // parse urlencoded request body
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors());
 
-app.use("/", router);
+app.use('/', router);
 
-server.listen(process.env.VITE_PORT ? +process.env.VITE_PORT : 4000, () => {
-  console.log("Server is running on port 4000");
-});
+if (process.env.NODE_ENV === 'production') {
+  const port = process.env.VITE_PORT ? +process.env.VITE_PORT : 4000;
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 export const viteNodeApp = app;
