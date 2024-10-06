a3335a27df4bc6e6518314b087b2c9781eb9de07
Noam Ben Shabat <nbs0556771785@n-b-solutions.com>
2024-10-06 11:08:03 +0300

    allow all origins in socket

--- server/socketio.ts
+++ server/socketio.ts
@@ -1,36 +1,32 @@
-import { IncomingMessage, Server as HttpServer, ServerResponse } from "http";
-import { Server as SocketioServer } from "socket.io";
+import { IncomingMessage, Server as HttpServer, ServerResponse } from 'http';
+import { Server as SocketioServer } from 'socket.io';
 
-import { CronJob } from "cron";
+import { CronJob } from 'cron';
 
-import ScheduleController from "./controller/scheduleController";
+import ScheduleController from './controller/scheduleController';
 
 let io;
 
 export const initSocketio = () => {
-  io = new SocketioServer(
-    process.env.VITE_SOCKET_PORT ? +process.env.VITE_SOCKET_PORT : 4001,
-    {
-      cors: {
-        origin: process.env.VITE_SITE_URL,
-        methods: ["GET", "POST"],
-      },
-    }
-  );
+  io = new SocketioServer(process.env.VITE_SOCKET_PORT ? +process.env.VITE_SOCKET_PORT : 4001, {
+    cors: {
+      origin: '*',
+    },
+  });
 
-  io.on("connection", (socket) => {
-    console.log("A user connected" + socket.id);
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
+  io.on('connection', (socket) => {
+    console.log('A user connected' + socket.id);
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
     });
   });
 
-  const job = new CronJob("* * * * *", async () => {
-    console.log("Running cron job to update rooms...");
+  const job = new CronJob('* * * * *', async () => {
+    console.log('Running cron job to update rooms...');
     const updatedStatuses = await ScheduleController.updateRoomStatuses();
 
     await ScheduleController.logBeforeShkiah();
-    io.emit("roomStatusUpdated", updatedStatuses);
+    io.emit('roomStatusUpdated', updatedStatuses);
   });
 
   job.start();
