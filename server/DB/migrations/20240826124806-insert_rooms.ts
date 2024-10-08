4a79240b2b6c3798609d4e758607a288e15552b1
Sara Cohen <sc0583218777@n-b-solutions.com>
2024-10-08 06:21:25 +0300

    migrations refactor

--- server/DB/migrations/20240826124806-insert_rooms.ts
+++ server/DB/migrations/20240826124806-insert_rooms.ts
@@ -1,28 +1,29 @@
-const fs = require("fs");
+const { readFileSync } = require("fs");
 const path = require("path");
 
+
 module.exports = {
-  async up(db, client) {
+  async up(db) {
     try {
-      // טוען נתוני JSON מהתיקיה 'data'
       const roomsData = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "..", "data", "rooms.json"))
+        readFileSync(
+          path.join(__dirname, "..", "data", "rooms.json")
+        ).toString()
       );
 
-      // מכניס נתונים ל-collections
       await db.collection("rooms").insertMany(roomsData);
 
-      console.log("Data has been inserted into the database");
+      console.log("Rooms (from json) inserted successfully!");
     } catch (error) {
       console.error("Error during migration up:", error);
     }
   },
 
-  async down(db, client) {
+  async down(db) {
     try {
       await db.collection("rooms").deleteMany({});
 
-      console.log("Data has been removed from the database");
+      console.log("Rooms deleted successfully!");
     } catch (error) {
       console.error("Error during migration down:", error);
     }
