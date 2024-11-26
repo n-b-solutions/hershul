26610603c62f0c8807b47ac039ec6262745ca7da
Sara Cohen <sc0583218777@n-b-solutions.com>
2024-11-26 08:49:55 +0200

    Add tooltip to isRoutineColumn for clarity on routine minyans

--- client/src/pages/minyanim/config/calendar.config.tsx
+++ client/src/pages/minyanim/config/calendar.config.tsx
@@ -1,16 +1,16 @@
 import { CheckCircle, XCircle } from '@phosphor-icons/react';
 import { Dayjs } from 'dayjs';
 
+import { LuachCalendarRowType } from '@/types/luach-minyan.type';
 import { CalendarRowType } from '@/types/minyans.type';
 import { ColumnDef } from '@/types/table.type';
-import { LuachCalendarRowType } from '@/types/luach-minyan.type';
 
 export interface CalendarTableProps {
   selectedDate: Dayjs;
   scrollAction: { isScroll: boolean; setIsScroll: React.Dispatch<React.SetStateAction<boolean>> };
 }
 
-export const isRoutineColumn: ColumnDef<CalendarRowType| LuachCalendarRowType> = {
+export const isRoutineColumn: ColumnDef<CalendarRowType | LuachCalendarRowType> = {
   editInputType: 'switch',
   valueForEdit: (row) => row?.isRoutine,
   name: 'Is Routine',
@@ -18,6 +18,7 @@ export const isRoutineColumn: ColumnDef<CalendarRowType| LuachCalendarRowType> =
   align: 'center',
   field: 'isRoutine',
   editable: true,
+  tooltip: 'Indicates whether the minyan will be valid on the same date in future years.',
   formatter: (row) => {
     if (row.isRoutine === undefined) return <></>;
     return row?.isRoutine ? <CheckCircle size={24} /> : <XCircle size={24} />;
