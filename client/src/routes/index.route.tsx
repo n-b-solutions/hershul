a2cb7d50f58ec07da23bf00a86d7dc81b5973c2f
Sara Cohen <sc0583218777@n-b-solutions.com>
2024-12-01 13:12:52 +0200

    Rename minyanim to settings

--- client/src/routes/index.route.tsx
+++ client/src/routes/index.route.tsx
@@ -2,7 +2,7 @@ import type { RouteObject } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 
 import { Home } from '@/pages/home/Home';
-import { Settings } from '@/pages/minyanim/Settings';
+import { Settings } from '@/pages/settings/Settings';
 import { NotFoundPage } from '@/pages/NotFound';
 import { Layout as MarketingLayout } from '@/components/marketing/layout/layout';
 import Header from '@/components/header/Header';
